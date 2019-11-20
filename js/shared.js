c998d5a8b065081a1983ebe9dbbefaa140dae675
juli4ka1998 <juli4ka1998@gmail.com>
2019-11-20 18:39:15 +0200

    Create portfolio sections effects and effects for menu

--- js/shared.js
+++ js/shared.js
@@ -1,21 +1,71 @@
 // MENU
 
 // Add event listener to close button
-let closeBtn = document.querySelector(".close-icon");
-closeBtn.addEventListener("click", () => {
-	// Hide menu and display main content
-	document.querySelector(".menu").style.display = "none";
-	document.querySelector(".main-content").style.display = "block";
-});
+// let closeBtn = document.querySelector(".close-icon");
+// closeBtn.addEventListener("click", () => {
+// 	// Hide menu and display main content
+// 	document.querySelector(".menu").style.display = "none";
+// 	document.querySelector(".main-content").style.display = "block";
+// });
 
+let currentPosition = 100;
 // Add event listener to menu button
 let menuBtn = document.querySelector(".menu-btn");
 menuBtn.addEventListener("click", () => {
-	menuBtn.classList.add("menu-btn-clicked");
+	menuBtn.classList.toggle("menu-btn-clicked");
+
+	// $(".main-header").addClass("header-menu");
+	// console.log($(".menu-btn").siblings());
+	// $(".menu-btn").siblings().addClass("hidden");
+	// $(".main-header").children[1].style.display = "none";
 
 	// Hide content and show menu
-	document.querySelector(".menu").style.display = "flex";
-	document.querySelector(".main-content").style.display = "none";
+	// setTimeout(document.querySelector(".menu").style.display = "flex", 500);
+
+	if(menuBtn.classList.contains("menu-btn-clicked")){
+		currentPosition = $(document).scrollTop();
+
+		document.querySelector(".menu").style.display = "flex";
+		$(".main-header").css({
+			"z-index": "auto"
+		});
+		$(".menu").delay(400).animate({
+			top: $(document).scrollTop(),
+			// opacity: 1
+		}, 300, "linear", function () {
+			$(".main-content").addClass("hidden");
+			$(".menu").css({
+				top: 0
+			});
+			$(document).scrollTop(0);
+			$(".main-header").css({
+				position: "absolute"
+			});
+		});
+	} else {
+		$(".main-content").removeClass("hidden");
+		$(".menu").css({
+			top: currentPosition
+		});
+		// $(".main-header").css({
+		// 	position: "fixed"
+		//
+		// });
+		$(document).scrollTop(currentPosition);
+		$(".menu").delay(400).animate({
+			top: "-140%",
+			// opacity: 1
+		}, 300, "linear", function () {
+			document.querySelector(".menu").style.display = "none";
+			$(".main-header").css({
+				position: "fixed",
+				"z-index": "300"
+			});
+		});
+	}
+
+	// document.querySelector(".main-content").style.display = "none";
+
 });
 
 // Show sub-menu
