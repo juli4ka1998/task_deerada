f33bb75b95ff0b5bc891b1911bec28ba49e6259a
juli4ka1998 <juli4ka1998@gmail.com>
2019-12-02 18:27:59 +0200

    Create case page

--- js/shared.js
+++ js/shared.js
@@ -12,12 +12,12 @@ let currentPosition = 0;
 // Add event listener to menu button
 let menuBtn = $(".menu-btn");
 let menuBtnBar = $(".menu-btn__bar");
-let menu = $(".menu");
+let menu = $(".menu-popup");
 let header = $(".main-header");
 let content = $(".main-content");
 menuBtn.click(function () {
 		//Change menu-btn
-	console.log(9090);
+	// console.log(9090);
 		menuBtn.toggleClass("menu-btn-clicked");
 
 		if(menuBtn.hasClass("menu-btn-clicked")){
@@ -25,26 +25,43 @@ menuBtn.click(function () {
 			menuBtnBar.removeClass("menu-btn-show");
 			currentPosition = $(document).scrollTop();
 
+
+
+
 			menu.removeClass("hidden");
+			let scroll = ($(document).width() - menu.width()) ;
 			header.css({
 				zIndex: "auto",
 				position: "absolute",
-				top: "+=" + currentPosition
+				top: "+=" + currentPosition,
+				marginRight: scroll
+				// right: "calc((100 / 1920 * 100vw) + (100vw - 100%))"
+			});
+
+			$("body").css({
+				height: "100%",
+				overflowY: "hidden"
+			});
+
+			menu.css({
+				paddingRight: scroll
 			});
 
 			menu.delay(400).animate({
 				top: currentPosition,
 			}, 500, "linear", function () {
 				content.addClass("hidden");
-
 				menu.css({
-					top: 0
+					top: 0,
+					// paddingRight: scroll
 				});
 
 				$(document).scrollTop(0);
 
 				header.css({
-					top: "-=" + currentPosition
+					top: "-=" + currentPosition,
+					// right: "calc((100 / 1920 * 100vw) + (100vw - 100%))"
+					// marginRight: scroll
 				});
 			});
 		} else {
@@ -53,11 +70,13 @@ menuBtn.click(function () {
 			content.removeClass("hidden");
 
 			menu.css({
-				top: currentPosition
+				top: currentPosition,
+				// paddingRight: 0
 			});
 
 			header.css({
-				top: "+=" + currentPosition
+				top: "+=" + currentPosition,
+				// marginRight: 0
 			});
 
 			$(document).scrollTop(currentPosition);
@@ -65,15 +84,25 @@ menuBtn.click(function () {
 			menu.delay(400).animate({
 				top: "-140%",
 			}, 500, "linear", function () {
+				menu.css({
+					paddingRight: 0
+				});
 				menu.addClass("hidden");
+
 
 				header.css({
 					position: "fixed",
 					"z-index": "300",
-					top: "-=" + currentPosition
+					top: "-=" + currentPosition,
+					marginRight: 0
+					// right: "calc(100 / 1920 * 100vw)"
 				});
 
 				menuBtnBar.addClass("menu-btn-show");
+				$("body").css({
+					height: "auto",
+					overflow: "auto"
+				});
 
 			});
 		}
@@ -130,16 +159,9 @@ owlCases.on('change.owl.carousel', function(event) {
 
 
 $(".big-btn-white").click(function () {
-	// console.log(9090);
-	// menuBtnBar.removeClass("menu-btn-show");
 	currentPosition = $(document).scrollTop();
 
 	$(".connect-popup").removeClass("hidden");
-	// header.css({
-	// 	zIndex: "auto",
-	// 	position: "absolute",
-	// 	top: "+=" + currentPosition
-	// });
 
 	$(".connect-popup").animate({
 		top: currentPosition,
@@ -151,8 +173,25 @@ $(".big-btn-white").click(function () {
 		});
 
 		$(document).scrollTop(0);
-
-		// header.css({
-		// 	top: "-=" + currentPosition
 		});
+});
+
+$(".popup .close-btn").click(function () {
+	// currentPosition = $(document).scrollTop();
+	content.removeClass("hidden");
+	$(".connect-popup").css({
+		top: currentPosition
+	});
+	$(document).scrollTop(currentPosition);
+
+	$(".connect-popup").animate({
+		top: "-140%",
+	}, 500, "linear", function () {
+
+		$(".connect-popup").addClass("hidden");
+
+
+
+		// $(document).scrollTop(0);
+	});
 });
